f469d8810fe099f37a3f185b897beab56c27ab46
gonglaji <997120724a>
2019-05-20 16:05:27 +0800

    wsdada

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    /**
+     *   Employee类：
+     *      ---Integer id 编号
+     *      ---String lastName  名字
+     *      ---int gender  性别(0,1)
+     *      ---String email 邮箱
+     *
+     *     在MySQL中创建对应的数据库表：
+     *     create table employee(
+     *       id int not null primary key auto_increment,
+     *       last_Name  varchar(20),
+     *       gender int(1),
+     *       email varchar(20));
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
